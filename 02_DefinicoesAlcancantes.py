4186fd1368f1ce5ba2cef5f7bf743461b8f57139
Francisco Breno <fbreno.dev@gmail.com>
2023-07-27 20:58:52 -0300

    add: definicoes alcancantes

---
+++ 02_DefinicoesAlcancantes.py
@@ -0,0 +1,28 @@
+import cfgClasses
+
+cfg = cfgClasses.CFGADA()
+idRegra = 1
+while True:
+    idRegras = []
+    regras = []
+    sucessores = []
+    
+    entrada = input()
+    if not entrada:
+        break
+    
+    entrada = entrada.split(' ')
+    id = int(entrada[0])
+    quantCod = int(entrada[1])
+
+    while quantCod > 0:
+        regras.append(input())
+        idRegras.append(idRegra)
+        idRegra += 1
+        quantCod -= 1
+    sucessores = [int(sucessor) for sucessor in input().split(' ')]
+
+    cfg.inserirBlocoBasico(id, idRegras, regras, sucessores)
+cfg.construirCFG()
+cfg.analisarCFG()
+# cfg.showCFG()
